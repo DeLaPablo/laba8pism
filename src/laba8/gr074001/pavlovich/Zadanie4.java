f6ba40cfe03d0ed989842b20e114ef37a397b7f0
DeLaPablo <arsen.titan@icloud.com>
2022-12-15 00:49:43 +0300

    I added function in a new class

--- src/laba8/gr074001/pavlovich/Zadanie4.java
+++ src/laba8/gr074001/pavlovich/Zadanie4.java
@@ -3,6 +3,21 @@ package laba8.gr074001.pavlovich;
 import java.util.Scanner;
 
 public class Zadanie4 {
+
+
+
+    public static void summachisel() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число 1:");
+        int a = scanner.nextInt();
+
+        System.out.println("Введите число 2:");
+        int b = scanner.nextInt();
+        int c = a + b;
+
+        System.out.println("Сумма:" + c);
+    }
 }
 
 
